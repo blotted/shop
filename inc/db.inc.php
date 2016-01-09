4718c52781f5bfb4f0b8848f29242fe59a30b89c
Igor Ivchenko <igorivchenko@inbox.ru>
2016-01-09 17:25:11 +0200

    first commit

--- inc/db.inc.php
+++ inc/db.inc.php
@@ -2,7 +2,7 @@
 header('Content-Type: text/html; charset=utf-8');
 define("DB_HOST", "localhost");
 define("DB_LOGIN", "root");
-define("DB_PASSWORD", "blottednasty");
+define("DB_PASSWORD", "");
 define("DB_NAME", "eshop");
 define("ORDERS_LOG", "orders.log");
 
